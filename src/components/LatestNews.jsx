7ec4fa4b97d124d8cb0a53873054af0b76d4214b
ssmahim01 <mahimzayn666@gmail.com>
2024-11-14 19:10:53 +0600

    Updated Latest News component

--- src/components/LatestNews.jsx
+++ src/components/LatestNews.jsx
@@ -5,13 +5,13 @@ const LatestNews = () => {
   return <div className="flex items-center gap-4 my-5 p-5 bg-[#F3F3F3] rounded-lg">
     <p className="bg-[#D72050] text-base-100 px-4 py-2">Latest</p>
     <Marquee pauseOnHover={true} speed={100} className="space-x-8">
-        <Link to="/news" className="text-gray-700 font-semibold">
+        <Link to="/category/01" className="text-gray-700 font-semibold">
         Match Highlights: Germany vs Spain — as it happened   !   Match Highlights: Germany vs Spain as...
         </Link>
-        <Link to="/news" className="text-gray-700 font-semibold">
+        <Link to="/category/02" className="text-gray-700 font-semibold">
         Match Highlights: Germany vs Spain — as it happened   !   Match Highlights: Germany vs Spain as...
         </Link>
-        <Link to="/news" className="text-gray-700 font-semibold">
+        <Link to="/category/03" className="text-gray-700 font-semibold">
         Match Highlights: Germany vs Spain — as it happened   !   Match Highlights: Germany vs Spain as...
         </Link>
     </Marquee>
