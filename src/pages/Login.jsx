509abb32281c15f81ecf0e401be4f1b01941f325
ssmahim01 <mahimzayn666@gmail.com>
2024-11-14 21:20:24 +0600

    Added Register page with Edited Login page and Updated router

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,9 +1,46 @@
+import { Link } from "react-router-dom";
+
 const Login = () => {
-    return (
-        <div>
-            
-        </div>
-    );
+  return (
+    <div className="min-h-screen flex justify-center items-center">
+      <div className="card bg-base-100 w-full max-w-lg shrink-0 rounded-none p-10">
+        <h2 className="text-2xl font-semibold text-center">Login your Account</h2>
+        <form className="card-body">
+          <div className="form-control">
+            <label className="label">
+              <span className="label-text">Email</span>
+            </label>
+            <input
+              type="email"
+              placeholder="email"
+              className="input input-bordered"
+              required
+            />
+          </div>
+          <div className="form-control">
+            <label className="label">
+              <span className="label-text">Password</span>
+            </label>
+            <input
+              type="password"
+              placeholder="password"
+              className="input input-bordered"
+              required
+            />
+            <label className="label">
+              <a href="#" className="label-text-alt link link-hover">
+                Forgot password?
+              </a>
+            </label>
+          </div>
+          <div className="form-control mt-6">
+            <button className="btn btn-neutral text-white font-semibold rounded-none">Login</button>
+          </div>
+        </form>
+        <p className="text-center font-semibold">Don't Have An Account ? <Link to="/auth/register" className="text-rose-500">Register</Link></p>
+      </div>
+    </div>
+  );
 };
 
 export default Login;
---
+++ src/pages/Register.jsx
@@ -0,0 +1,64 @@
+import { Link } from "react-router-dom";
+
+const Register = () => {
+    return (
+        <div className="min-h-screen flex justify-center items-center">
+        <div className="card bg-base-100 w-full max-w-lg shrink-0 rounded-none p-10">
+          <h2 className="text-2xl font-semibold text-center">Register your Account</h2>
+          <form className="card-body">
+            <div className="form-control">
+              <label className="label">
+                <span className="label-text">Name</span>
+              </label>
+              <input
+                type="text"
+                placeholder="Name"
+                className="input input-bordered"
+                required
+              />
+            </div>
+            <div className="form-control">
+              <label className="label">
+                <span className="label-text">Photo URL</span>
+              </label>
+              <input
+                type="text"
+                placeholder="Photo URL"
+                className="input input-bordered"
+                required
+              />
+            </div>
+            <div className="form-control">
+              <label className="label">
+                <span className="label-text">Email</span>
+              </label>
+              <input
+                type="email"
+                placeholder="email"
+                className="input input-bordered"
+                required
+              />
+            </div>
+            <div className="form-control">
+              <label className="label">
+                <span className="label-text">Password</span>
+              </label>
+              <input
+                type="password"
+                placeholder="password"
+                className="input input-bordered"
+                required
+              />
+            </div>
+            <div className="form-control mt-6">
+              <button className="btn btn-neutral text-white font-semibold rounded-none">Register</button>
+            </div>
+          </form>
+
+          <p className="text-center font-semibold">Already Have An Account ? <Link to="/auth/login" className="text-blue-500">Login</Link></p>
+        </div>
+      </div>
+    );
+};
+
+export default Register;
